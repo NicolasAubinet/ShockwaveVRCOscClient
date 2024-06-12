242fe73ffed7cf62e70bb047b7a966c537f59bca
Nicolas Aubinet <n.aubinet@gmail.com>
2024-06-12 19:01:40 +0200

    Porting of the VRChat client Console App to Maui

---
+++ ShockwaveVRCOscClient/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace ShockwaveVRCOscClient;
+
+public partial class MainPage : ContentPage
+{
+	// int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+	{
+        ShockwaveVRChat.Program.StartClient();
+		// count++;
+
+		// if (count == 1)
+			// CounterBtn.Text = $"Clicked {count} time";
+		// else
+			// CounterBtn.Text = $"Clicked {count} times";
+
+		// SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
---
+++ ShockwaveVRCOscClient/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ShockwaveVRCOscClient;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
